1afcd6424087875523bf04dca314d045fd849a77
Fellipe Araujo <araujo.fellipe@hotmail.com>
2022-04-12 18:32:24 -0300

    Add ormconfig file and others configurations to deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,14 +1,22 @@
+import 'reflect-metadata';
 import { FishLog } from '../models/fishLog';
 import { DataSource } from 'typeorm';
 
 export const connection = new DataSource({
   type: 'postgres',
-  host: 'db',
-  port: 5434,
-  username: 'root',
-  password: 'admin',
-  database: 'log',
+  host: process.env.POSTGRES_HOST || 'db',
+  port: Number(process.env.POSTGRES_PORT) || 5434,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [FishLog],
   synchronize: true,
   logging: false,
+  extra: process.env.POSTGRES_HOST
+    ? {
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : null,
 });
