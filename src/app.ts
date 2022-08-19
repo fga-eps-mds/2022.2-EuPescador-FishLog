c2d3caa0a053cec112f79cbf3784405f6791d1d6
André Freitas <andreabenf@gmail.com>
2022-08-19 14:40:30 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
+
 import 'reflect-metadata';
+
 import cors from 'cors';
 import express from 'express';
 import router from './routes/router';
@@ -6,12 +8,12 @@ import router from './routes/router';
 require('dotenv').config();
 
 const app = express();
+
 app.disable('x-powered-by');
 
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb' }));
+app.use(express.json());
+app.use(cors());
 
 app.use(router);
-app.use(cors());
 
 export default app;
