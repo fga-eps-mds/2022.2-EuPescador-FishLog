abf98d184379da589d4c505730c9d2d5030be753
lucasgandac <lucasgandac@hotmail.com>
2022-08-26 12:16:02 -0300

    Ajuste para possivel foto maior

--- src/app.ts
+++ src/app.ts
@@ -11,8 +11,8 @@ const app = express();
 
 app.disable('x-powered-by');
 
-app.use(express.json({ limit: '15mb' }));
-app.use(express.urlencoded({ limit: '15mb' }));
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb' }));
 
 app.use(express.json());
 app.use(cors());
